2538fae9986664556f4e9e9445dd51426faef362
NaasaX <besancon.marcelin@hotmail.com>
2026-04-07 09:58:09 +0200

    fix : duplication route

--- sql/db_user.data.sql
+++ sql/db_user.data.sql
@@ -2,5 +2,5 @@ INSERT INTO users (id, email, password)
 VALUES (
     'e79fa90f-29a4-4c81-99c7-ebc33a892689', 
     'jean@photopro.com', 
-    '$2a$12$oXSM6zW6bxYMvzphdZyQBe6YPbZLmHfi9CrlP1Tcp7K4YUC0fF8cq'
+    '$2a$12$B2GORiOmfBe76499u0SxwODg/oYGC756Tuz9rWuMjsyhkOHUxzWhq' -- mdp : jean
 );
